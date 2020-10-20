649b7b65acb6056c462e698ae7ef65643fb0dd0c
Vitor <cid.vitor@hotmail.com>
2020-10-20 16:45:35 -0300

    add ransack and filter methods

--- ruby_answer/app/controllers/phones_controller.rb
+++ ruby_answer/app/controllers/phones_controller.rb
@@ -1,6 +1,6 @@
 class PhonesController < ApplicationController
-
-	def index
+	def index		
+		@phones = Phone.ransack(filter_params).result
 	end
 
 	def upload
@@ -9,6 +9,10 @@ class PhonesController < ApplicationController
 
 	def update	
 		Phone.import(params[:phone][:file])
+		redirect_to "/phones"
 	end
 
+	def filter_params
+		params.permit(:manufacturer_eq,:model_eq,:sale_modality_eq)
+	end
 end
--- ruby_answer/app/models/phone.rb
+++ ruby_answer/app/models/phone.rb
@@ -2,7 +2,7 @@ class Phone < ApplicationRecord
 	require 'csv'
 
 	def self.import(file)	
-		CSV.foreach(file.path, headers: true) do |row|
+		CSV.foreach(file.path, headers: true) do |row|	
 			Phone.create! row.to_hash
 		end
 	end
