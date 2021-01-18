47617a773c8d8198b3abf33d59733db0af2052c4
calcantara <claudio.ta@gmail.com>
2021-01-18 08:53:11 -0300

    Realizei ajustes no layout, iniciei os testes automatizados.

--- test/controllers/cell_phones_controller_test.rb
+++ test/controllers/cell_phones_controller_test.rb
@@ -1,14 +1,25 @@
 require 'test_helper'
 
 class CellPhonesControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @cellphone = cell_phones(:motorola)
+  end
+
   test "should get index" do
     get cell_phones_index_url
+      get cell_phones_url, params: {cell_phone:
+          {manufacturer: Faker::Name.name, model: Faker::Name.name, carrier_plan_type: Faker::Name.name }
+      }
     assert_response :success
   end
 
-  test "should create import" do
-    get cell_phones_import_url
-    assert_response :success
+  test "should import cell_phone" do
+    assert_difference('cell_phones.count') do
+      post cell_phones_url, params: {cell_phone: {manufacturer: Faker::Name.name }}
+    end
+
+    assert_response :import
   end
 
 end
