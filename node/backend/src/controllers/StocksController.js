b0b18c72a701f5b33f5b054134a13c1a6522936b
Melquias Ribeiro <melquias10.mr@gmail.com>
2021-02-25 17:57:56 -0300

    feat: backend routes to all features

--- node/backend/src/controllers/StocksController.js
+++ node/backend/src/controllers/StocksController.js
@@ -1,5 +1,140 @@
+/* eslint-disable array-callback-return */
+const api = require('../utils/api');
+
 module.exports = {
-  async index(req, res) {
-    return res.json('ok');
+  async actualQuote(req, res) {
+    const { symbol } = req.params;
+    if (symbol) {
+      const { data } = await api.get(`query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=A15TFI52OO3BO9KP`);
+      if (data['Global Quote'].length === 0) {
+        return res.status(400).json({ error: 'Tem certeza que os dados informados estão corretos?' });
+      }
+      const Quote = {
+        name: data['Global Quote']['01. symbol'],
+        lastPrice: parseFloat(data['Global Quote']['05. price']),
+        pricedAt: new Date(
+          data['Global Quote']['07. latest trading day'],
+        ).toISOString(),
+      };
+      return res.json(Quote);
+    }
+    return res.status(400).json({ error: 'forneça um ativo para consulta' });
+  },
+  async gainsProjection(req, res) {
+    const { symbol } = req.params;
+    const { purchasedAmount, purchasedAt, finalDate } = req.query;
+    const { data } = await api.get(
+      `query?function=TIME_SERIES_DAILY&symbol=${symbol}&outputsize=full&apikey=&apikey=A15TFI52OO3BO9KP`,
+    );
+
+    if (data['Time Series (Daily)'][purchasedAt] !== undefined) {
+      const purchasePriceAtDate = Number.parseFloat(
+        data['Time Series (Daily)'][purchasedAt]['4. close'],
+      );
+
+      if (finalDate) {
+        if (data['Time Series (Daily)'][finalDate
+        ]) {
+          const priceAtFinalDate = Number.parseFloat(
+            data['Time Series (Daily)'][finalDate
+            ]['4. close'],
+          );
+
+          const gains = purchasedAmount * priceAtFinalDate
+                - purchasedAmount * purchasePriceAtDate;
+          return res.json({
+            name: symbol,
+            purchasedAmount,
+            purchasedAt: new Date(purchasedAt).toISOString(),
+            finalDate: new Date(finalDate).toISOString(),
+            purchasePriceAtDate,
+            priceAtFinalDate,
+            capitalGains: `R$ ${gains}`,
+          });
+        }
+        return res.status(400).json({ error: 'não há dados disponivés para projeções com esta data' });
+      }
+      const priceAtLastRefreshed = Number.parseFloat(
+        data['Time Series (Daily)'][
+          Object.keys(data['Time Series (Daily)'])[0]
+        ]['4. close'],
+      );
+      const lastRefreshed = data['Meta Data']['3. Last Refreshed'];
+      const gains = purchasedAmount * priceAtLastRefreshed
+        - purchasedAmount * purchasePriceAtDate;
+      return res.json({
+        name: symbol,
+        purchasedAmount,
+        purchasedAt: new Date(purchasedAt).toISOString(),
+        lastRefreshed: new Date(lastRefreshed).toISOString(),
+        priceAtDate: purchasePriceAtDate,
+        lastPrice: priceAtLastRefreshed,
+        capitalGains: `R$ ${gains}`,
+      });
+    }
+    return res.status(400).json({ error: 'Tem certeza que os dados informados estão corretos?' });
+  },
+  async historical(req, res) {
+    const { symbol } = req.params;
+    const { from, to } = req.query;
+    const { data } = await api.get(
+      `query?function=TIME_SERIES_DAILY&symbol=${symbol}&outputsize=full&apikey=&apikey=A15TFI52OO3BO9KP`,
+    );
+
+    if (data['Time Series (Daily)'][from] && data['Time Series (Daily)'][to]) {
+      const startDate = new Date(from);
+      const endDate = new Date(to);
+      const history = [];
+      const days = data['Time Series (Daily)'];
+      Object.keys(days).map((day) => {
+        const valueDate = new Date(day);
+        if (valueDate >= startDate && valueDate <= endDate) {
+          history.push({
+            opening: days[day]['1. open'],
+            low: days[day]['3. low'],
+            high: days[day]['2. high'],
+            closing: days[day]['4. close'],
+            pricedAt: new Date(day).toISOString(),
+          });
+        }
+      });
+      return res.json({ name: symbol, prices: history });
+    }
+    return res.status(400).json({ error: 'os dados para estas datas não estão disponiveis' });
+  },
+  async compare(req, res) {
+    const { symbol } = req.params;
+    const { stocks } = req.body;
+    if (stocks) {
+      const data = [];
+      const listPrices = [];
+      stocks.push(symbol);
+      stocks.forEach((stock) => {
+        data.push(
+          api.get(
+            `query?function=GLOBAL_QUOTE&symbol=${stock}&apikey=A15TFI52OO3BO9KP`,
+          ),
+        );
+      });
+      const response = await Promise.all(data);
+      response.map((item) => {
+        if (item.data['Global Quote']['01. symbol']) {
+          listPrices.push({
+            name: item.data['Global Quote']['01. symbol'],
+            lastPrice: Number.parseFloat(
+              item.data['Global Quote']['05. price'],
+            ),
+            pricedAt: new Date(
+              item.data['Global Quote']['07. latest trading day'],
+            ).toISOString(),
+          });
+        } else {
+          listPrices.push({ error: 'ativo não encontrado' });
+        }
+      });
+
+      return res.json(listPrices);
+    }
+    return res.status(400).json({ error: 'é necessario enviar uma ou mais ações para a  comparação' });
   },
 };
--- node/backend/src/routes.js
+++ node/backend/src/routes.js
@@ -3,6 +3,9 @@ const StocksController = require('./controllers/StocksController');
 
 const routes = new Router();
 
-routes.get('/stocks', StocksController.index);
+routes.get('/stocks/:symbol/quote', StocksController.actualQuote);
+routes.get('/stocks/:symbol/gains', StocksController.gainsProjection);
+routes.get('/stocks/:symbol/history', StocksController.historical);
+routes.post('/stocks/:symbol/compare', StocksController.compare);
 
 module.exports = routes;
