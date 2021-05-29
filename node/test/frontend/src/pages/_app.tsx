3bc62837d86514c5eda2e1f5e4b5d865a674ca50
Pedro H. Queiroz <delkawy@gmail.com>
2021-05-29 13:07:14 -0300

    add styled components & eslint

---
+++ node/test/frontend/src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Stock SPA</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
---
+++ node/test/frontend/src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 10px;
+  }
+
+  body, html, #__next {
+    min-height: 100vh;
+  }
+
+  body {
+    font-family: 'Poppins', ---apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 1.6rem;
+  }
+`
