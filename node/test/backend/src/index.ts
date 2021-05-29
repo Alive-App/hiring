b7da294000c074b7103aa5881483c3f384a05dc5
Pedro H. Queiroz <delkawy@gmail.com>
2021-05-29 03:39:43 -0300

    add cors

--- node/test/backend/src/index.ts
+++ node/test/backend/src/index.ts
@@ -1,12 +1,14 @@
 import 'dotenv/config'
 import express, { Request, NextFunction, Response } from 'express'
 import 'express-async-errors'
+import cors from 'cors'
 
 import { AppError } from './errors/AppError'
 import routes from './routes'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
