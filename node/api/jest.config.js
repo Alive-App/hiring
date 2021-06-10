bc0def9b640a2d82684622830194daddd58e5ea1
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-10 20:58:05 -0300

    chore: add jest

---
+++ node/api/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  preset: "ts-jest",
+  testEnvironment: "node",
+};
