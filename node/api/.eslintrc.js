d2c762ea4ea95c42761aaace7f09ede2cc5632f4
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-10 21:12:38 -0300

    chore: add jest plugin

--- node/api/.eslintrc.js
+++ node/api/.eslintrc.js
@@ -1,19 +1,15 @@
 module.exports = {
   env: {
     es2021: true,
-    node: true
+    node: true,
+    'jest/globals': true
   },
-  extends: [
-    'standard'
-  ],
+  extends: ['standard'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module'
   },
-  plugins: [
-    '@typescript-eslint'
-  ],
-  rules: {
-  }
+  plugins: ['@typescript-eslint', 'jest'],
+  rules: {}
 }
