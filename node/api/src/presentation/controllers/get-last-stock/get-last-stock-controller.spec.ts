267b464a758e3028a508940263d1f09159aae4ff
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-10 21:13:13 -0300

    test GetLastStockController return 400 if stockName not provided

---
+++ node/api/src/presentation/controllers/get-last-stock/get-last-stock-controller.spec.ts
@@ -0,0 +1,26 @@
+import { ParamNotProvidedError } from 'presentation/errors/param-not-provided-error'
+import { badRequest } from 'presentation/helpers/http'
+import { HttpRequest } from 'presentation/protocols/http-request'
+import { GetLastStockController } from './get-last-stock-controller'
+
+const makeFakeRequest = (): HttpRequest => ({
+  params: {
+    stockName: 'any_stock_name'
+  }
+})
+
+const makeSut = () => {
+  const sut = new GetLastStockController()
+
+  return { sut }
+}
+
+describe('GetLastStockController', () => {
+  test('should return 400 if stockName not provided', async () => {
+    const { sut } = makeSut()
+    const request = makeFakeRequest()
+    request.params.stockName = ''
+    const response = await sut.handle(request)
+    expect(response).toEqual(badRequest(new ParamNotProvidedError('stockName')))
+  })
+})
