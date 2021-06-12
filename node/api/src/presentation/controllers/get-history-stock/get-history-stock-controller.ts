eee684fc9fb51e01b6e72e58aa105185705d97c4
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-12 12:33:36 -0300

    feat: ensure GetHistoryStockController return 400 if fromDate invalid

--- node/api/src/presentation/controllers/get-history-stock/get-history-stock-controller.ts
+++ node/api/src/presentation/controllers/get-history-stock/get-history-stock-controller.ts
@@ -1,3 +1,5 @@
+import { IsoDateValidation } from 'data/protocols/iso-date-validation'
+import { ParamInvalidError } from 'presentation/errors/param-invalid-error'
 import { ParamNotProvidedError } from 'presentation/errors/param-not-provided-error'
 import { badRequest } from 'presentation/helpers/http'
 import { Controller } from 'presentation/protocols/controller'
@@ -5,9 +7,16 @@ import { HttpRequest } from 'presentation/protocols/http-request'
 import { HttpResponse } from 'presentation/protocols/http-response'
 
 export class GetHistoryStockController implements Controller {
+  constructor (
+    private readonly isoDateValidation: IsoDateValidation
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.query.fromDate) {
       return badRequest(new ParamNotProvidedError('fromDate'))
+    }
+    if (!this.isoDateValidation.isIsoDateValid(httpRequest.query.fromDate)) {
+      return badRequest(new ParamInvalidError('fromDate'))
     }
     if (!httpRequest.query.toDate) {
       return badRequest(new ParamNotProvidedError('toDate'))
