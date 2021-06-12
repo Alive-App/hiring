61edbe167dc2589c494a4457ea088843a7b203f8
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-12 12:22:20 -0300

    feat: ensure GetHistoryStockController return 400 if stockName not provided

---
+++ node/api/src/presentation/controllers/get-history-stock/get-history-stock-controller.ts
@@ -0,0 +1,11 @@
+import { ParamNotProvidedError } from 'presentation/errors/param-not-provided-error'
+import { badRequest } from 'presentation/helpers/http'
+import { Controller } from 'presentation/protocols/controller'
+import { HttpRequest } from 'presentation/protocols/http-request'
+import { HttpResponse } from 'presentation/protocols/http-response'
+
+export class GetHistoryStockController implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    return badRequest(new ParamNotProvidedError('stockName'))
+  }
+}
