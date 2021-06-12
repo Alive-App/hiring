88b5f1ea7da77d5e6fd393e82e6b21f10026b1e1
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-12 13:49:43 -0300

    feat: ensure GetHistoryStockController return 500 if isoDateValidation throws

--- node/api/src/presentation/controllers/get-history-stock/get-history-stock-controller.ts
+++ node/api/src/presentation/controllers/get-history-stock/get-history-stock-controller.ts
@@ -2,7 +2,7 @@ import { IsoDateValidation } from 'data/protocols/iso-date-validation'
 import { GetHistoryStockUsecase } from 'domain/usecases/get-history-stock-usecase'
 import { ParamInvalidError } from 'presentation/errors/param-invalid-error'
 import { ParamNotProvidedError } from 'presentation/errors/param-not-provided-error'
-import { badRequest } from 'presentation/helpers/http'
+import { badRequest, serverError } from 'presentation/helpers/http'
 import { Controller } from 'presentation/protocols/controller'
 import { HttpRequest } from 'presentation/protocols/http-request'
 import { HttpResponse } from 'presentation/protocols/http-response'
@@ -14,21 +14,25 @@ export class GetHistoryStockController implements Controller {
   ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { stockName } = httpRequest.params
-    const { fromDate, toDate } = httpRequest.query
-    if (!fromDate) {
-      return badRequest(new ParamNotProvidedError('fromDate'))
+    try {
+      const { stockName } = httpRequest.params
+      const { fromDate, toDate } = httpRequest.query
+      if (!fromDate) {
+        return badRequest(new ParamNotProvidedError('fromDate'))
+      }
+      if (!this.isoDateValidation.isIsoDateValid(fromDate)) {
+        return badRequest(new ParamInvalidError('fromDate'))
+      }
+      if (!toDate) {
+        return badRequest(new ParamNotProvidedError('toDate'))
+      }
+      if (!this.isoDateValidation.isIsoDateValid(toDate)) {
+        return badRequest(new ParamInvalidError('toDate'))
+      }
+      await this.getHistoryStockUsecase.getHistory(stockName, fromDate, toDate)
+      return badRequest(new ParamNotProvidedError('stockName'))
+    } catch (err) {
+      return serverError()
     }
-    if (!this.isoDateValidation.isIsoDateValid(fromDate)) {
-      return badRequest(new ParamInvalidError('fromDate'))
-    }
-    if (!toDate) {
-      return badRequest(new ParamNotProvidedError('toDate'))
-    }
-    if (!this.isoDateValidation.isIsoDateValid(toDate)) {
-      return badRequest(new ParamInvalidError('toDate'))
-    }
-    await this.getHistoryStockUsecase.getHistory(stockName, fromDate, toDate)
-    return badRequest(new ParamNotProvidedError('stockName'))
   }
 }
