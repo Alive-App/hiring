dc59dba40ccb98e39ba0aaad0ac159c9b8c78303
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-12 11:58:01 -0300

    refactor: rename factory to factories folder

--- node/api/src/main/config/routes.ts
+++ node/api/src/main/config/routes.ts
@@ -1,6 +1,6 @@
 import { Application } from 'express'
 import { expressControllerAdapter } from 'main/adapters/express-controller-adapter'
-import { makeGetLastStockController } from 'main/factory/make-get-last-stock-controller'
+import { makeGetLastStockController } from 'main/factories/make-get-last-stock-controller'
 
 export const setupRoutes = (app: Application) => {
   app.get('/stocks/:stockName/quote', expressControllerAdapter(makeGetLastStockController()))
