3670cab8f40cb4a8efe8ac8e6da9c3a1db23c226
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-12 14:26:32 -0300

    test: ensure ServiceGetHistoryStockUsecase calls GetHistoryStockService with correct values

---
+++ node/api/src/data/usecases/get-history-stock/service-get-history-stock.spec.ts
@@ -0,0 +1,48 @@
+import { GetHistoryStockService } from 'data/protocols/get-history-stock-service'
+import { HistoryStockModel, HistoryStockPricingModel } from 'domain/models/history-stock-model'
+import { ServiceGetHistoryStockUsecase } from './service-get-history-stock'
+
+const stockName = 'IBM'
+
+const fromDate = new Date(2020, 1, 1)
+
+const toDate = new Date(2020, 2, 1)
+
+const makeFakeHistoryStockPricingModel = (): HistoryStockPricingModel => ({
+  closing: 10,
+  high: 10,
+  low: 10,
+  opening: 10,
+  pricedAt: 'any_priced_at'
+})
+
+const makeFakeHistoryStockModel = (): HistoryStockModel => ({
+  name: 'any_name',
+  prices: [makeFakeHistoryStockPricingModel()]
+})
+
+const makeGetHistoryStockServiceStub = () => {
+  class GetHistoryStockServiceStub implements GetHistoryStockService {
+    async getHistoryStock (stockName: string, fromDate: Date, toDate: Date): Promise<HistoryStockModel> {
+      return makeFakeHistoryStockModel()
+    }
+  }
+
+  return new GetHistoryStockServiceStub()
+}
+
+const makeSut = () => {
+  const getHistoryStockServiceStub = makeGetHistoryStockServiceStub()
+  const sut = new ServiceGetHistoryStockUsecase(getHistoryStockServiceStub)
+
+  return { sut, getHistoryStockServiceStub }
+}
+
+describe('ServiceGetHistoryStockUsecase', () => {
+  test('should call getHistoryStockService with correct values', async () => {
+    const { sut, getHistoryStockServiceStub } = makeSut()
+    const getHistoryStockSpy = jest.spyOn(getHistoryStockServiceStub, 'getHistoryStock')
+    await sut.getHistory(stockName, fromDate, toDate)
+    expect(getHistoryStockSpy).toHaveBeenLastCalledWith(stockName, fromDate, toDate)
+  })
+})
