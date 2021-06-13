9bf594d4f22ac62bffb0515a2eace4c73f341365
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-12 22:47:52 -0300

    feat: ensure ServiceGetStockGainsUsecase calls getStockByDateService with correct values

--- node/api/src/data/usecases/get-stock-gains/service-get-stock-gains-usecase.ts
+++ node/api/src/data/usecases/get-stock-gains/service-get-stock-gains-usecase.ts
@@ -1,14 +1,17 @@
 import { GetLastStockService } from 'data/protocols/get-last-stock-service'
+import { GetStockByDateService } from 'data/protocols/get-stock-by-date-service'
 import { StockGainsModel } from 'domain/models/stock-gains-model'
 import { GetStockGainsUsecase } from 'domain/usecases/get-stock-gains-usecase'
 
 export class ServiceGetStockGainsUsecase implements GetStockGainsUsecase {
   constructor (
-    private readonly getLastStockService: GetLastStockService
+    private readonly getLastStockService: GetLastStockService,
+    private readonly getStockByDateService: GetStockByDateService
   ) {}
 
   async getGains (stockName: string, purchasedAmount: number, purchasedAt: Date): Promise<StockGainsModel> {
     await this.getLastStockService.getLastStock(stockName)
+    await this.getStockByDateService.getStockByDate(stockName, purchasedAt)
     return null as any
   }
 }
