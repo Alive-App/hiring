3365a11ff78efd59391a6aca1d308930c56a9975
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-13 16:51:24 -0300

    feat: load stock data on api

--- node/web/src/pages/home/components/stock-item/index.tsx
+++ node/web/src/pages/home/components/stock-item/index.tsx
@@ -4,9 +4,17 @@ import { useStock } from '../../../../hooks/use-stock'
 import { Button } from '../../../../components/button'
 
 import { ActionsContainer } from './styles'
+import { useCallback, useEffect, useState } from 'react'
+import { api } from '../../../../services/api'
 
 export interface StockItemProps {
   stockName: string;
+}
+
+export interface StockApi {
+  lastPrice: number;
+  name: string;
+  pricedAt: string;
 }
 
 export const StockItem = ({ stockName }: StockItemProps) => {
@@ -16,11 +24,31 @@ export const StockItem = ({ stockName }: StockItemProps) => {
   const { removeStock } = useStock()
 
   /**
+   * States
+   */
+  const [stock, setStock] = useState({} as StockApi)
+
+  /**
+   * Functions
+   */
+  const loadStockOnApi = useCallback(async () => {
+    const { data } = await api.get<StockApi>(`/stocks/${stockName}/quote`)
+    setStock(data)
+  }, [stockName])
+
+  /**
    * Handles
    */
   const handleRemoveClick = () => {
     removeStock(stockName)
   }
+
+  /**
+   * Effects
+   */
+  useEffect(() => {
+    loadStockOnApi()
+  }, [loadStockOnApi])
 
   /**
    * Returns
@@ -31,12 +59,14 @@ export const StockItem = ({ stockName }: StockItemProps) => {
       style={{ padding: 10, margin: 10, minWidth: 300 }}
     >
       <Typography variant="h6">{stockName}</Typography>
-      <Typography variant="body1">Preço atual: </Typography>
-      <Typography variant="body1">Atualizado em: </Typography>
+      <Typography variant="body1">Preço atual: {stock.lastPrice}</Typography>
+      <Typography variant="body1">Atualizado em: {stock.pricedAt}</Typography>
 
       <ActionsContainer>
         <Button>Ver detalhes</Button>
-        <Button variant="outlined" onClick={handleRemoveClick}>Excluir</Button>
+        <Button variant="outlined" onClick={handleRemoveClick}>
+          Excluir
+        </Button>
       </ActionsContainer>
     </Paper>
   )
