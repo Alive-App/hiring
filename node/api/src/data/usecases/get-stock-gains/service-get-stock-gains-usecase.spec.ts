54a98254543507728e9f05cae88a40f77fad8bc8
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-12 22:47:57 -0300

    test: ensure ServiceGetStockGainsUsecase calls getStockByDateService with correct values

--- node/api/src/data/usecases/get-stock-gains/service-get-stock-gains-usecase.spec.ts
+++ node/api/src/data/usecases/get-stock-gains/service-get-stock-gains-usecase.spec.ts
@@ -1,8 +1,16 @@
 import { GetLastStockService } from 'data/protocols/get-last-stock-service'
+import { GetStockByDateService } from 'data/protocols/get-stock-by-date-service'
 import { LastStockModel } from 'domain/models/last-stock-model'
+import { StockByDateModel } from 'domain/models/stock-by-date-model'
 import { ServiceGetStockGainsUsecase } from './service-get-stock-gains-usecase'
 
 const makeFakeLastStockModel = (): LastStockModel => ({
+  lastPrice: 10,
+  name: 'any_name',
+  pricedAt: 'any_priced_at'
+})
+
+const makeFakeStockByDateModel = (): StockByDateModel => ({
   lastPrice: 10,
   name: 'any_name',
   pricedAt: 'any_priced_at'
@@ -17,11 +25,23 @@ const makeGetLastStockServiceStub = () => {
 
   return new GetLastStockServiceStub()
 }
+
+const makeGetStockByDateServiceStub = () => {
+  class GetStockByDateServiceStub implements GetStockByDateService {
+    async getStockByDate (stockName: string, date: Date): Promise<StockByDateModel> {
+      return makeFakeStockByDateModel()
+    }
+  }
+
+  return new GetStockByDateServiceStub()
+}
+
 const makeSut = () => {
   const getLastStockServiceStub = makeGetLastStockServiceStub()
-  const sut = new ServiceGetStockGainsUsecase(getLastStockServiceStub)
+  const getStockByDateServiceStub = makeGetStockByDateServiceStub()
+  const sut = new ServiceGetStockGainsUsecase(getLastStockServiceStub, getStockByDateServiceStub)
 
-  return { sut, getLastStockServiceStub }
+  return { sut, getLastStockServiceStub, getStockByDateServiceStub }
 }
 
 describe('ServiceGetStockGainsUsecase', () => {
@@ -39,5 +59,13 @@ describe('ServiceGetStockGainsUsecase', () => {
     jest.spyOn(getLastStockServiceStub, 'getLastStock').mockRejectedValueOnce(new Error())
     const promise = sut.getGains('any_name', 100, date)
     await expect(promise).rejects.toThrow()
+  })
+
+  test('should call getStockByDateService with correct value', async () => {
+    const { sut, getStockByDateServiceStub } = makeSut()
+    const date = new Date()
+    const getStockByDatekSpy = jest.spyOn(getStockByDateServiceStub, 'getStockByDate')
+    await sut.getGains('any_name', 100, date)
+    expect(getStockByDatekSpy).toHaveBeenLastCalledWith('any_name', date)
   })
 })
