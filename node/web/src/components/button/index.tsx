78546dc74ab742f92543678295cd6cdee8c3ef68
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-13 11:49:54 -0300

    feat: add onClick in button

--- node/web/src/components/button/index.tsx
+++ node/web/src/components/button/index.tsx
@@ -1,11 +1,18 @@
-import { ReactNode } from 'react'
+import { ReactNode, MouseEventHandler } from 'react'
 import { Button as MuiButton } from '@material-ui/core'
 
 export interface ButtonProps {
   children: ReactNode;
-  variant?: 'contained' | 'outlined'
+  variant?: 'contained' | 'outlined';
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-export const Button = ({ children, variant = 'contained' }: ButtonProps) => (
-  <MuiButton variant={variant} color='primary'>{children}</MuiButton>
+export const Button = ({
+  children,
+  onClick,
+  variant = 'contained'
+}: ButtonProps) => (
+  <MuiButton variant={variant} color="primary" onClick={onClick}>
+    {children}
+  </MuiButton>
 )
