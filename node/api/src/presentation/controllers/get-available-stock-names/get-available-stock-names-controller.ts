4ae0550e216d4fc4083933b9f8513bf3dfb7a9bb
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-13 09:54:47 -0300

    feat: ensure GetAvailableStockNamesController calls  GetAvailableStockNamesUsecase with correct value

--- node/api/src/presentation/controllers/get-available-stock-names/get-available-stock-names-controller.ts
+++ node/api/src/presentation/controllers/get-available-stock-names/get-available-stock-names-controller.ts
@@ -1,3 +1,4 @@
+import { GetAvailableStockNamesUsecase } from 'domain/usecases/get-available-stock-names-usecase'
 import { ParamNotProvidedError } from 'presentation/errors/param-not-provided-error'
 import { badRequest } from 'presentation/helpers/http'
 import { Controller } from 'presentation/protocols/controller'
@@ -5,7 +6,12 @@ import { HttpRequest } from 'presentation/protocols/http-request'
 import { HttpResponse } from 'presentation/protocols/http-response'
 
 export class GetAvailableStockNamesController implements Controller {
+  constructor (
+    private readonly getAvailableStockNamesUsecase: GetAvailableStockNamesUsecase
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.getAvailableStockNamesUsecase.getStockNames(httpRequest.query.search)
     return badRequest(new ParamNotProvidedError('search'))
   }
 }
