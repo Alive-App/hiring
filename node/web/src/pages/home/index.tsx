7a289b69316c6334a95b66d5664a3cecc098e80d
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-13 11:58:43 -0300

    feat: add function to change home page to add-stock page

--- node/web/src/pages/home/index.tsx
+++ node/web/src/pages/home/index.tsx
@@ -1,3 +1,5 @@
+import { useHistory } from 'react-router-dom'
+
 import { Container } from '../../components/container'
 import { Header } from '../../components/header'
 import { Button } from '../../components/button'
@@ -5,27 +7,44 @@ import { Button } from '../../components/button'
 import { StockList } from './styles'
 import { StockItem } from './components/stock-item'
 
-export const Home = () => (
-  <Container>
-    <Header>
-      <Button>Incluir ação</Button>
-    </Header>
+export const Home = () => {
+  /**
+   * Hooks
+   */
+  const { push } = useHistory()
 
-    <StockList>
-      <StockItem />
-      <StockItem />
-      <StockItem />
-      <StockItem />
-      <StockItem />
-      <StockItem />
-      <StockItem />
-      <StockItem />
-      <StockItem />
-      <StockItem />
-      <StockItem />
-      <StockItem />
-      <StockItem />
-      <StockItem />
-    </StockList>
-  </Container>
-)
+  /**
+   * Handles
+   */
+  const handleAddStockClick = () => {
+    push('/add-stock')
+  }
+
+  /**
+   * Returns
+   */
+  return (
+    <Container>
+      <Header>
+        <Button onClick={handleAddStockClick}>Incluir ação</Button>
+      </Header>
+
+      <StockList>
+        <StockItem />
+        <StockItem />
+        <StockItem />
+        <StockItem />
+        <StockItem />
+        <StockItem />
+        <StockItem />
+        <StockItem />
+        <StockItem />
+        <StockItem />
+        <StockItem />
+        <StockItem />
+        <StockItem />
+        <StockItem />
+      </StockList>
+    </Container>
+  )
+}
