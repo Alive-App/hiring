ddfe717113132f4c08b687db98795d4073b19531
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-12 22:26:48 -0300

    feat: ensure GetStockGainsController return 500 if getStockGainsUsecase throws

--- node/api/src/presentation/controllers/get-stock-gains/get-stock-gains-controller.ts
+++ node/api/src/presentation/controllers/get-stock-gains/get-stock-gains-controller.ts
@@ -1,6 +1,6 @@
 import { GetStockGainsUsecase } from 'domain/usecases/get-stock-gains-usecase'
 import { ParamNotProvidedError } from 'presentation/errors/param-not-provided-error'
-import { badRequest } from 'presentation/helpers/http'
+import { badRequest, serverError } from 'presentation/helpers/http'
 import { Controller } from 'presentation/protocols/controller'
 import { HttpRequest } from 'presentation/protocols/http-request'
 import { HttpResponse } from 'presentation/protocols/http-response'
@@ -11,16 +11,20 @@ export class GetStockGainsController implements Controller {
   ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.query.purchasedAmount) {
-      return badRequest(new ParamNotProvidedError('purchasedAmount'))
-    }
-    if (!httpRequest.query.purchasedAt) {
-      return badRequest(new ParamNotProvidedError('purchasedAt'))
-    }
+    try {
+      if (!httpRequest.query.purchasedAmount) {
+        return badRequest(new ParamNotProvidedError('purchasedAmount'))
+      }
+      if (!httpRequest.query.purchasedAt) {
+        return badRequest(new ParamNotProvidedError('purchasedAt'))
+      }
 
-    const purchasedAtDate = new Date(httpRequest.query.purchasedAt)
+      const purchasedAtDate = new Date(httpRequest.query.purchasedAt)
 
-    await this.getStockGainsUsecase.getGains(httpRequest.params.stockName, httpRequest.query.purchasedAmount, purchasedAtDate)
-    return badRequest(new ParamNotProvidedError('stockName'))
+      await this.getStockGainsUsecase.getGains(httpRequest.params.stockName, httpRequest.query.purchasedAmount, purchasedAtDate)
+      return badRequest(new ParamNotProvidedError('stockName'))
+    } catch (err) {
+      return serverError()
+    }
   }
 }
