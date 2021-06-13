7022b697de34abf03286cebdb679440e046e0876
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-13 16:31:54 -0300

    feat: add remove stock function

--- node/web/src/pages/home/components/stock-item/index.tsx
+++ node/web/src/pages/home/components/stock-item/index.tsx
@@ -1,21 +1,43 @@
 import { Paper, Typography } from '@material-ui/core'
 
+import { useStock } from '../../../../hooks/use-stock'
 import { Button } from '../../../../components/button'
+
 import { ActionsContainer } from './styles'
 
 export interface StockItemProps {
   stockName: string;
 }
 
-export const StockItem = ({ stockName }: StockItemProps) => (
-  <Paper variant="outlined" style={{ padding: 10, margin: 10, minWidth: 300 }}>
-    <Typography variant="h6">{stockName}</Typography>
-    <Typography variant="body1">Preço ação</Typography>
-    <Typography variant="body1">Data atualização</Typography>
+export const StockItem = ({ stockName }: StockItemProps) => {
+  /**
+   * Hooks
+   */
+  const { removeStock } = useStock()
 
-    <ActionsContainer>
-      <Button>Ver detalhes</Button>
-      <Button variant="outlined">Excluir</Button>
-    </ActionsContainer>
-  </Paper>
-)
+  /**
+   * Handles
+   */
+  const handleRemoveClick = () => {
+    removeStock(stockName)
+  }
+
+  /**
+   * Returns
+   */
+  return (
+    <Paper
+      variant="outlined"
+      style={{ padding: 10, margin: 10, minWidth: 300 }}
+    >
+      <Typography variant="h6">{stockName}</Typography>
+      <Typography variant="body1">Preço ação</Typography>
+      <Typography variant="body1">Data atualização</Typography>
+
+      <ActionsContainer>
+        <Button>Ver detalhes</Button>
+        <Button variant="outlined" onClick={handleRemoveClick}>Excluir</Button>
+      </ActionsContainer>
+    </Paper>
+  )
+}
