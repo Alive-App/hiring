889cf7719811d6b5887508c1b1b86b1b9c599057
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-12 22:54:56 -0300

    feat: ensure ServiceGetStockGainsUsecase return a stockGains on success

--- node/api/src/data/usecases/get-stock-gains/service-get-stock-gains-usecase.ts
+++ node/api/src/data/usecases/get-stock-gains/service-get-stock-gains-usecase.ts
@@ -7,11 +7,22 @@ export class ServiceGetStockGainsUsecase implements GetStockGainsUsecase {
   constructor (
     private readonly getLastStockService: GetLastStockService,
     private readonly getStockByDateService: GetStockByDateService
-  ) {}
+  ) { }
 
   async getGains (stockName: string, purchasedAmount: number, purchasedAt: Date): Promise<StockGainsModel> {
-    await this.getLastStockService.getLastStock(stockName)
-    await this.getStockByDateService.getStockByDate(stockName, purchasedAt)
-    return null as any
+    const lastStock = await this.getLastStockService.getLastStock(stockName)
+    const stockByDate = await this.getStockByDateService.getStockByDate(stockName, purchasedAt)
+
+    const valueAtDate = purchasedAmount * stockByDate.lastPrice
+    const lastValue = purchasedAmount * lastStock.lastPrice
+
+    return {
+      name: stockName,
+      purchasedAmount: purchasedAmount,
+      purchasedAt: stockByDate.pricedAt,
+      priceAtDate: stockByDate.lastPrice,
+      lastPrice: lastStock.lastPrice,
+      capitalGains: lastValue - valueAtDate
+    }
   }
 }
