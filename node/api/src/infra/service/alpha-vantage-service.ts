57bc09231d2bbc6176d925f480dde86b267d9331
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-13 08:38:31 -0300

    fix: compare date

--- node/api/src/infra/service/alpha-vantage-service.ts
+++ node/api/src/infra/service/alpha-vantage-service.ts
@@ -154,14 +154,20 @@ export class AlphaVantageService implements GetLastStockService, GetHistoryStock
 
   async getStockByDate (stockName: string, date: Date): Promise<StockByDateModel> {
     const data = await this.timeSeriesDaily(stockName)
+    const day = date.getDay()
+    const month = date.getMonth()
+    const year = date.getFullYear()
     const stockByDate: StockByDateModel = {} as StockByDateModel
 
     const keys = Object.keys(data['Time Series (Daily)'])
 
     for (const key of keys) {
       const stockDate = new Date(key)
+      const dayStock = stockDate.getDay()
+      const monthStock = stockDate.getMonth()
+      const yearStock = stockDate.getFullYear()
 
-      if (stockDate.getTime() === date.getTime()) {
+      if (day === dayStock && month === monthStock && year === yearStock) {
         const stock = data['Time Series (Daily)'][key]
         stockByDate.lastPrice = Number(stock['4. close'])
         stockByDate.name = stockName
