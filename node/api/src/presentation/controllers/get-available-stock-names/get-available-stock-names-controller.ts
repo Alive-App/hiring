8e1d0d0512f2a630627fcee100286c21d24d054a
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-13 09:57:58 -0300

    feat: ensure GetAvailableStockNamesController return 200 on success

--- node/api/src/presentation/controllers/get-available-stock-names/get-available-stock-names-controller.ts
+++ node/api/src/presentation/controllers/get-available-stock-names/get-available-stock-names-controller.ts
@@ -1,6 +1,6 @@
 import { GetAvailableStockNamesUsecase } from 'domain/usecases/get-available-stock-names-usecase'
 import { ParamNotProvidedError } from 'presentation/errors/param-not-provided-error'
-import { badRequest, serverError } from 'presentation/helpers/http'
+import { badRequest, ok, serverError } from 'presentation/helpers/http'
 import { Controller } from 'presentation/protocols/controller'
 import { HttpRequest } from 'presentation/protocols/http-request'
 import { HttpResponse } from 'presentation/protocols/http-response'
@@ -8,12 +8,15 @@ import { HttpResponse } from 'presentation/protocols/http-response'
 export class GetAvailableStockNamesController implements Controller {
   constructor (
     private readonly getAvailableStockNamesUsecase: GetAvailableStockNamesUsecase
-  ) {}
+  ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.getAvailableStockNamesUsecase.getStockNames(httpRequest.query.search)
-      return badRequest(new ParamNotProvidedError('search'))
+      if (!httpRequest.query.search) {
+        return badRequest(new ParamNotProvidedError('search'))
+      }
+      const stockNames = await this.getAvailableStockNamesUsecase.getStockNames(httpRequest.query.search)
+      return ok(stockNames)
     } catch (err) {
       return serverError()
     }
