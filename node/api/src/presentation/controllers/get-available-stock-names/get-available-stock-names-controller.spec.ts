12385a84ebb5ee0ba1e7d33687171defa3f7ef58
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-13 09:54:53 -0300

    test: ensure GetAvailableStockNamesController calls  GetAvailableStockNamesUsecase with correct value

--- node/api/src/presentation/controllers/get-available-stock-names/get-available-stock-names-controller.spec.ts
+++ node/api/src/presentation/controllers/get-available-stock-names/get-available-stock-names-controller.spec.ts
@@ -1,3 +1,4 @@
+import { GetAvailableStockNamesUsecase } from 'domain/usecases/get-available-stock-names-usecase'
 import { ParamNotProvidedError } from 'presentation/errors/param-not-provided-error'
 import { badRequest } from 'presentation/helpers/http'
 import { HttpRequest } from 'presentation/protocols/http-request'
@@ -9,10 +10,21 @@ const makeFakeRequest = (): HttpRequest => ({
   }
 })
 
-const makeSut = () => {
-  const sut = new GetAvailableStockNamesController()
+const makeGetAvailableStockNamesUsecaseStub = () => {
+  class GetAvailableStockNamesUsecaseStub implements GetAvailableStockNamesUsecase {
+    async getStockNames (search: string): Promise<string[]> {
+      return ['stock_name_1', 'stock_name_2']
+    }
+  }
 
-  return { sut }
+  return new GetAvailableStockNamesUsecaseStub()
+}
+
+const makeSut = () => {
+  const getAvailableStockNamesUsecaseStub = makeGetAvailableStockNamesUsecaseStub()
+  const sut = new GetAvailableStockNamesController(getAvailableStockNamesUsecaseStub)
+
+  return { sut, getAvailableStockNamesUsecaseStub }
 }
 
 describe('GetAvailableStockNamesController', () => {
@@ -22,5 +34,13 @@ describe('GetAvailableStockNamesController', () => {
     request.query.search = ''
     const response = await sut.handle(request)
     expect(response).toEqual(badRequest(new ParamNotProvidedError('search')))
+  })
+
+  test('should call getAvailableStockNamesUsecase with correct values', async () => {
+    const { sut, getAvailableStockNamesUsecaseStub } = makeSut()
+    const getStockNamesSpy = jest.spyOn(getAvailableStockNamesUsecaseStub, 'getStockNames')
+    const request = makeFakeRequest()
+    await sut.handle(request)
+    expect(getStockNamesSpy).toHaveBeenLastCalledWith(request.query.search)
   })
 })
