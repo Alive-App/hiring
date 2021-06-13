3cb0d35a8089e5ed6d90333fde5e9cdee7bc35cc
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-12 22:11:08 -0300

    feat: ensure GetStockGainsController return 400 if purchasedAmount not provided

--- node/api/src/presentation/controllers/get-stock-gains/get-stock-gains-controller.ts
+++ node/api/src/presentation/controllers/get-stock-gains/get-stock-gains-controller.ts
@@ -6,6 +6,9 @@ import { HttpResponse } from 'presentation/protocols/http-response'
 
 export class GetStockGainsController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    if (!httpRequest.query.purchasedAmount) {
+      return badRequest(new ParamNotProvidedError('purchasedAmount'))
+    }
     return badRequest(new ParamNotProvidedError('stockName'))
   }
 }
