fead5cebd78897bd66925467ecc6aa909a52f711
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-13 16:08:17 -0300

    feat: organize SearchResultItem

--- node/web/src/pages/add-stock/components/search-result-item/index.tsx
+++ node/web/src/pages/add-stock/components/search-result-item/index.tsx
@@ -1,11 +1,33 @@
 import { Paper } from '@material-ui/core'
 
+import { Button } from '../../../../components/button'
+
 export interface SearchResultItemProps {
   stockName: string;
 }
 
-export const SearchResultItem = ({ stockName }:SearchResultItemProps) => (
-  <Paper variant="outlined" style={{ padding: 10, margin: 10 }}>
-    {stockName}
-  </Paper>
-)
+export const SearchResultItem = ({ stockName }: SearchResultItemProps) => {
+  /**
+   * Handles
+   */
+  const handleAddStockClick = () => {}
+
+  /**
+   * Returns
+   */
+  return (
+    <Paper
+      variant="outlined"
+      style={{
+        padding: 10,
+        margin: 10,
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+      }}
+    >
+      {stockName}
+      <Button onClick={handleAddStockClick}>Adicionar</Button>
+    </Paper>
+  )
+}
