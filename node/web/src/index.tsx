0831762544ddd9688b8dc9f507174281d86cc6e6
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-13 11:08:26 -0300

    chore: add eslint

--- node/web/src/index.tsx
+++ node/web/src/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Home} from './pages/Home';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Home } from './pages/Home'
+import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
-reportWebVitals();
+reportWebVitals()
