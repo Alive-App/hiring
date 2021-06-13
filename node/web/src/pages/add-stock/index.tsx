e31cb2470266ea9aa53b60b0bad08782f574f831
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-13 11:43:41 -0300

    feat: add add-stock page

---
+++ node/web/src/pages/add-stock/index.tsx
@@ -0,0 +1,8 @@
+import { Container } from '../../components/container'
+import { Header } from '../../components/header'
+
+export const AddStock = () => (
+  <Container>
+    <Header>Hitalo</Header>
+  </Container>
+)
