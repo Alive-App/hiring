a0454ae38ddd1722612b166cf2743ae66c8db4e8
Hitalo Schraiber Loose <hitaloose@gmail.com>
2021-06-13 12:01:10 -0300

    feat: add back button to add-stock page

--- node/web/src/pages/add-stock/index.tsx
+++ node/web/src/pages/add-stock/index.tsx
@@ -1,8 +1,30 @@
+import { useHistory } from 'react-router-dom'
+
 import { Container } from '../../components/container'
 import { Header } from '../../components/header'
+import { Button } from '../../components/button'
 
-export const AddStock = () => (
-  <Container>
-    <Header>Hitalo</Header>
-  </Container>
-)
+export const AddStock = () => {
+  /**
+   * Hooks
+   */
+  const { goBack } = useHistory()
+
+  /**
+   * Handles
+   */
+  const handleBackClick = () => {
+    goBack()
+  }
+
+  /**
+   * Returns
+   */
+  return (
+    <Container>
+      <Header>
+        <Button onClick={handleBackClick}>Voltar</Button>
+      </Header>
+    </Container>
+  )
+}
